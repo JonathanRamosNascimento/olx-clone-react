1c451edffd671892e74d72ea83d6568a63849ac0
Jonathan Ramos Nascimento <ramos.was.jonathan@gmail.com>
2019-11-26 14:58:46 -0300

    Estruturando inicialmente o template do projeto

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
+import { Template } from './components/MainComponent';
+import Header from './components/partials/Header';
+import Footer from './components/partials/Footer';
+
 import Routes from './Routes';
 
 const Page = (props) => {
   return (
     <BrowserRouter>
-      <Routes />
+      <Template>
+        <Header />
+
+        <Routes />
+
+        <Footer />
+      </Template>
     </BrowserRouter>
   );
 }
