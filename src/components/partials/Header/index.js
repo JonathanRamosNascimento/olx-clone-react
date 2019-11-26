9cb82545cfb20ee4875d287cd4e02f86064f96f9
Jonathan Ramos Nascimento <ramos.was.jonathan@gmail.com>
2019-11-26 16:23:32 -0300

    criando o cookie para salvar se esta logado ou nao e modificar o menu de acordo com a resposta

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { HeaderArea } from './styled';
 
+import { isLogged } from '../../../helpers/AuthHandler';
+
 const Header = () => {
+    let logged = isLogged();
+
     return (
         <HeaderArea>
             <div className="container">
@@ -15,15 +19,32 @@ const Header = () => {
                 </div>
                 <nav>
                     <ul>
-                        <li>
-                            <Link to="">Login</Link>
-                        </li>
-                        <li>
-                            <Link to="">Cadastrar</Link>
-                        </li>
-                        <li>
-                            <Link to="" className="button">Poste um anúncio</Link>
-                        </li>
+                        {logged &&
+                            <>
+                                <li>
+                                    <Link to="/my-account">Minha Conta</Link>
+                                </li>
+                                <li>
+                                    <Link to="/logout">Sair</Link>
+                                </li>
+                                <li>
+                                    <Link to="/post-an-ad" className="button">Poste um anúncio</Link>
+                                </li>
+                            </>
+                        }
+                        {!logged &&
+                            <>
+                                <li>
+                                    <Link to="/signin">Login</Link>
+                                </li>
+                                <li>
+                                    <Link to="/signup">Cadastrar</Link>
+                                </li>
+                                <li>
+                                    <Link to="/signin" className="button">Poste um anúncio</Link>
+                                </li>
+                            </>
+                        }
                     </ul>
                 </nav>
             </div>
