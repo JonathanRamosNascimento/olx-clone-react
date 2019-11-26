00f63f867d2329cb3a3ac3ef212b24bb73aacda9
Jonathan Ramos Nascimento <ramos.was.jonathan@gmail.com>
2019-11-26 15:57:39 -0300

    criando o menu header

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { HeaderArea } from './styled';
 
 const Header = () => {
     return (
         <HeaderArea>
-            ...
+            <div className="container">
+                <div className="logo">
+                    <Link to="/">
+                        <span className="logo-1">O</span>
+                        <span className="logo-2">L</span>
+                        <span className="logo-3">X</span>
+                    </Link>
+                </div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="">Cadastrar</Link>
+                        </li>
+                        <li>
+                            <Link to="" className="button">Poste um anúncio</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </HeaderArea>
     );
 }
