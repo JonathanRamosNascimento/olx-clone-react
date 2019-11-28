dfba3a4027b21deae5c9549340c15227a3e842f8
Jonathan Ramos Nascimento <ramos.was.jonathan@gmail.com>
2019-11-27 21:54:53 -0300

    Criando campo de busca

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,7 +12,18 @@ const Page = () => {
         <>
             <SearchArea>
                 <PageContainer>
-                    ...
+                    <div className="searchBox">
+                        <form method="GET" action="/ads">
+                            <input type="text" name="q" placeholder="O que você procura?"/>
+                            <select name="state">
+
+                            </select>
+                            <button>Pesquisar</button>
+                        </form>
+                    </div>
+                    <div className="categoryList">
+
+                    </div>
                 </PageContainer>
             </SearchArea>
             <PageContainer>
--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -1,5 +1,51 @@
 import styled from 'styled-components';
 
-export const SearchArea = styled.div``;
+export const SearchArea = styled.div`
+background-color: #DDD;
+border-bottom: #CCC;
+padding: 20px 0;
+
+.searchBox {
+    background-color: #9BB83C;
+    padding: 20px 15px;
+    border-radius: 5px;
+    box-shadow: 1px 1px 1px 0.3px rgba(0, 0, 0, 0.2);
+
+    form {
+        flex: 1;
+        display: flex;
+
+        input, select {
+            height: 40px;
+            border: 0;
+            border-radius: 5px;
+            outline: 0;
+            font-size: 15px;
+            color: #000;
+            margin-right: 20px;
+        }
+
+        input {
+            flex: 1;
+            padding: 0 10px;
+        }
+
+        select {
+            width: 100px;
+        }
+
+        button {
+            background-color: #49AEEF;
+            font-size: 15px;
+            border: 0;
+            border-radius: 5px;
+            color: #FFF;
+            height: 40px;
+            padding: 0 20px;
+            cursor: pointer;
+        }
+    }
+}
+`;
 
 export const PageArea = styled.div``;
